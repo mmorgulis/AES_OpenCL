09fca597ae4c8859a7ba7ce478bd48acf1623267
Fabio <curradogianfigliazzi03@gmail.com>
2025-06-18 22:50:49 +0200

    rm out/build

--- out/build/x64-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/VisualStudio/repos/AES_OpenCL
-# Build directory: D:/VisualStudio/repos/AES_OpenCL/out/build/x64-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tests")
--- out/build/x64-debug/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/VisualStudio/repos/AES_OpenCL/src
-# Build directory: D:/VisualStudio/repos/AES_OpenCL/out/build/x64-debug/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
